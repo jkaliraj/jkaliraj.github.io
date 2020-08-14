791fc45856742e75cdf794ad9bad6054f94b2364
KR <kaliraj.jeganathan@gmail.com>
2020-08-14 12:00:14 +0530

    Add logo image and update Header component to use logo from config

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,55 +1,54 @@
+import React, { Component } from "react";
+import { Link } from "react-router";
 
-import React, { Component } from 'react'
-import { Link } from 'react-router'
+import classnames from "classnames";
 
-import classnames from 'classnames'
-
-import Navigation from '../Navigation'
-import style from './style.css'
-import store from '../../store'
-import grid from '../../assets/css/grid.css'
-import imagepath from '../../utils/imagepath'
+import Navigation from "../Navigation";
+import style from "./style.css";
+import store from "../../store";
+import grid from "../../assets/css/grid.css";
+import imagepath from "../../utils/imagepath";
 
 class Header extends Component {
   getFile(name) {
-    const { data } = store.getState()
+    const { data } = store.getState();
 
-    let file = null
+    let file = null;
 
     if (data[`${name}@2x`]) {
       file = (
         <img
-          src={imagepath(data[`${name}@2x`].localFile, 'full')}
+          src={imagepath(data[`${name}@2x`].localFile, "full")}
           width={data[`${name}@2x`].width / 2}
-          height={data[`${name}@2x`].height / 2} />
-      )
+          height={data[`${name}@2x`].height / 2}
+        />
+      );
     }
 
     if (data[`${name}`]) {
       file = (
         <img
-          src={imagepath(data[`${name}`].localFile, 'full')}
+          src={imagepath(data[`${name}`].localFile, "full")}
           width={data[`${name}`].width}
-          height={data[`${name}`].height} />
-      )
+          height={data[`${name}`].height}
+        />
+      );
     }
 
-    return file
+    return file;
   }
 
   render() {
-    let logoDefault = this.getFile('logo')
-    let logoInverse = this.getFile('logo.inverse')
-    let logoActive = this.getFile('logo.active')
-
-    let logo = (
-      <div className={style.logoWrap}>
-        <div className={style.logoDefault}>{logoDefault}</div>
-        <div className={style.logoInverse}>{logoInverse}</div>
-        <div className={style.logoActive}>{logoActive}</div>
-      </div>
-    )
-
+    const getConfig = require("../../utils/getConfig").default;
+    const config = getConfig();
+    let logo = null;
+    if (config.logo) {
+      logo = (
+        <div className={style.logoWrap}>
+          <img src={config.logo} width={64} height={64} alt="Logo" />
+        </div>
+      );
+    }
     return (
       <header className={style.header}>
         <div className={classnames(style.headerInner, grid.container)}>
@@ -59,8 +58,8 @@ class Header extends Component {
           <Navigation location={this.props.location} className={style.nav} />
         </div>
       </header>
-    )
+    );
   }
 }
 
-export default Header
+export default Header;
